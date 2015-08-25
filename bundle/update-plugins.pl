152f877464176bb850cac578048e772989947134
Daniel Asher Resnick <danwizard208@gmail.com>
2015-08-24 21:55:58 -0500

    Fix plugins and update scripts
    Modeline in the plugin list and the regexen in the scripts were
    slightly broken.

--- bundle/update-plugins.pl
+++ bundle/update-plugins.pl
@@ -6,9 +6,9 @@ foreach $plugin (@plugins)
 {
     # Plugins lines start with +<space>; ignore other lines
     # (this was added to enable a vim modeline in the plugins file)
-    if($plugin ~= /^+ /)
+    if($plugin ~= /^\+ /)
     {
-        $plugin =~ s/^+ //;
+        $plugin =~ s/^\+ //;
         chomp($plugin);
         # Plugins stored as a name and url separated by a number of tabs
         ($name, $url) = split /\t+/,$plugin;
